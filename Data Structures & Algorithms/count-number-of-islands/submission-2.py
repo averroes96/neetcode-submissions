ae2109ab0c7504b9372cf615e7bdb24a0e0104d1
Ada Meceffeuk <ada.meceffeuk@etu.usthb.dz>
2026-09-02 18:59:45 +0100

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        islands = 0
+
+        def _neighbors(pos):
+            row, col = pos
+            return (row - 1, col), (row + 1, col), (row, col + 1), (row, col - 1)
+
+        def DFS(position):
+            row, col = position
+            value = grid[row][col]
+
+            if value == "0":
+                return
+            
+            grid[row][col] = "0"
+            neighbors = _neighbors(position)
+
+            for neighbor in neighbors:
+                row, col = neighbor
+                if (row == n or col == m or row < 0 or col < 0):
+                    continue
+                
+                DFS(neighbor)
+        
+        for row in range(n):
+            for col in range(m):
+                if grid[row][col] == "1":
+                    islands += 1
+                    DFS((row, col)) 
+
+        return islands
